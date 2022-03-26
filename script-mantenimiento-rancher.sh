fea773e9a8d150ca91cd242a2bda7bb7208f5a9d
Johan Sebastian Giraldo H <johansebastiangh@gmail.com>
2022-03-26 00:06:09 -0500

    second commit

---
+++ script-mantenimiento-rancher.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo docker rm -f $(docker ps -qa)
+
+sudo docker rmi -f $(docker images -q)
+
+sudo docker volume rm $(docker volume ls -q)
+
+sudo docker system prune -a -y
+
+sudo for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
+
+sudo rm -rfv /etc/ceph \
+       /etc/cni \
+       /etc/kubernetes \
+       /opt/cni \
+       /opt/rke \
+       /run/secrets/kubernetes.io \
+       /run/calico \
+       /run/flannel \
+       /var/lib/calico \
+       /var/lib/etcd \
+       /var/lib/cni \
+       /var/lib/kubelet \
+       /var/lib/rancher/rke/log \
+       /var/log/containers \
+       /var/log/kube-audit \
+       /var/log/pods \
+       /var/run/calico
